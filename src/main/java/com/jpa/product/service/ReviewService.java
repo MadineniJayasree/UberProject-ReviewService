416e58b60ba26fb5d7571e30a660fec526b73da8
Madineni Jayasree <jayasreemadineni20@gmail.com>
2025-09-09 18:19:13 +0530

    added service layer

--- src/main/java/com/jpa/product/service/ReviewService.java
+++ src/main/java/com/jpa/product/service/ReviewService.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class ReviewService implements CommandLineRunner {
@@ -15,8 +16,12 @@ public class ReviewService implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception{
         System.out.println("*******");
-        Review r = Review.builder().content("Amazing ride quality").createdAt(new Date()).rating(5.0).updatedAt(new Date()).build();
+        Review r = Review.builder().content("Amazing ride quality").createdAt(new Date()).rating(4.0).updatedAt(new Date()).build();
         System.out.println(r);
         reviewRepository.save(r); // executes sql query
+        List<Review> reviews = reviewRepository.findAll();
+        reviews.stream().forEach(review -> System.out.println(review.getContent()));
+        reviews.stream().forEach(Review::getContent);
+//        reviewRepository.deleteById(2L);
     }
 }
